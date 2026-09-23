5a1a0115ada6d45de7c88e4840fcbeda94419168
David Paul <david.paul@springernature.com>
2026-09-23 15:28:50 +0100

    Improve data formatting

--- data/reports.js
+++ data/reports.js
@@ -15,10 +15,12 @@ export const reports = {
 	authorFeedbackTitle: 'Feedback for author(s)',
 	editorFeedbackTitle: 'Feedback for editor',
 	ratingTitle: 'How useful did you find this report?',
-	items: [{
-		reviewerName: 'Megan Haran',
-		reviewDate: formatDate('Wed, 26 Aug 2020 08:20:45 GMT'),
-		authorFeedbackText: 'I am not sure whether the Kerr nonlinearity does not enhanced unwanted non RWA features and destroys the effect. But this is just a side remark. It is a nice and well-written piece of scientific work.',
-		editorFeedbackText: 'Although this paper looks interesting, I have raised some points that the author needs to address before it can be accepted for publication.',
-	}]
+	items: [
+		{
+			reviewerName: 'Megan Haran',
+			reviewDate: formatDate('Wed, 26 Aug 2020 08:20:45 GMT'),
+			authorFeedbackText: 'I am not sure whether the Kerr nonlinearity does not enhanced unwanted non RWA features and destroys the effect. But this is just a side remark. It is a nice and well-written piece of scientific work.',
+			editorFeedbackText: 'Although this paper looks interesting, I have raised some points that the author needs to address before it can be accepted for publication.',
+		}
+	]
 };
